a23b3a999cbf0bfb72899ac63c33e39c53379ef8
Alessandro Chelli <alessandrochelli@gmail.com>
2016-09-06 14:05:50 +0200

    filemanager

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var config = require("./config.js");
 //var TelegramBot = require('node-telegram-bot-api');
 //var localtunnel = require('localtunnel');
 console.log(config)
-var url =""; 
+var url ="";
 var tunnelFlag = false;
 
 var exec = require('child_process').exec;
@@ -25,9 +25,9 @@ app.use(function (req, res, next) {
         //Apio.user.email.push(req.session.email);
         next();
     }
-});
+});*/
 app.use('/filemanager', fileManager());
-*/
+
 
 app.get('/foo', function(req, res, next) {
   res.send('bar');
@@ -38,16 +38,16 @@ app.listen();
 var tunnelInit = function(){
 	var tunnel = localtunnel(port, function(err, tunnel) {
 	if (err){
-	    
+
 	} else {
 	    // the assigned public url for your tunnel
 	    // i.e. https://abcdefgjhij.localtunnel.me
 	    tunnelFlag = true;
 	    url = tunnel.url;
-	    
+
 	}
 	});
-	
+
 	tunnel.on('close', function() {
 	 // tunnels are closed
 	 tunnelFlag = false;
@@ -66,27 +66,27 @@ var bot = new TelegramBot(token, {polling: true});
   bot.sendMessage(fromId, resp);
 });*/
 /*
-bot.on('message', function (msg) {                                              
-  var chatId = msg.chat.id;                                                     
-  console.log(msg);                                                             
-  if(msg.text=="reboot"){                                                       
-     bot.sendMessage(chatId, "Ok, I will reboot!")                           
-     setTimeout(function(){                                                 
-            exec("sudo reboot", function (error, stdout, stderr) {          
-            if (error || stderr) {                                          
-            console.log("exec error: " + error || stderr);                  
-        } else if (stdout) {                                                
-            console.log("Board is rebooting in a while, please wait");      
-        }                                                                   
-      },1000);                                                                                                                                    
-     })                                                                     
-  }                                                                             
-  else {                                                                        
-    if(url===""){                                                         
-          bot.sendMessage(chatId, "Your tunnel is offline, write me in a while"); 
-          tunnelInit();                                                   
-    } else {                                                              
-          bot.sendMessage(chatId, "Hi, here's the address of your tunnel "+url);                                                                         
-    }                                                                     
-  }                                                                                             
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  console.log(msg);
+  if(msg.text=="reboot"){
+     bot.sendMessage(chatId, "Ok, I will reboot!")
+     setTimeout(function(){
+            exec("sudo reboot", function (error, stdout, stderr) {
+            if (error || stderr) {
+            console.log("exec error: " + error || stderr);
+        } else if (stdout) {
+            console.log("Board is rebooting in a while, please wait");
+        }
+      },1000);
+     })
+  }
+  else {
+    if(url===""){
+          bot.sendMessage(chatId, "Your tunnel is offline, write me in a while");
+          tunnelInit();
+    } else {
+          bot.sendMessage(chatId, "Hi, here's the address of your tunnel "+url);
+    }
+  }
 });*/
